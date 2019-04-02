d2590591cdfb16cd23fa0cd2e38966ea7f463a4a
wolfram74 <phaugen@gmail.com>
2019-04-02 10:58:20 -0600

    set up an unique id method on messages

--- src/message.py
+++ src/message.py
@@ -7,14 +7,22 @@ def decoding(message_string):
     return json.loads(message_string.decode())
 
 class Message():
-    def __init__(self, payload):
+    def __init__(self, payload, persist=True):
         self.payload = payload
+        self.persistent=persist
 
     def encode(self):
         return encoding(self.payload)
 
     def destination(self):
         return tuple(self.payload['destination'])
+
+    def m_uid(self):
+        return (
+            self.payload['message_type'],
+            self.payload['sender_id'],
+            self.payload['message_id']
+            )
 
     @classmethod
     def fromByteString(cls, byte_string):
