2bc01417fe250d07d86538c6da8f728bad6f3beb
wolfram74 <phaugen@gmail.com>
2019-04-08 14:24:20 -0600

    some re-working of integration tests

--- src/integration_test.py
+++ src/integration_test.py
@@ -1,7 +1,19 @@
 from lobby import Lobby
 from player import Player
+from message import Message
 import subprocess
 from time import time
+
+def fake_new_game(player_obj):
+    player.request_new_game()
+    pass
+
+def fake_submit_word(player_obj):
+    player.network_obj.enque(Message({
+        'message_type':'submit_secret_word',
+        'secret_word':'quine'
+        }))
+    pass
 
 if __name__ == '__main__':
     player = Player.set_up_controller()
@@ -10,10 +22,15 @@ if __name__ == '__main__':
         )
     player.request_player_id()
     start_time = time()
-    benchmarks = [(1,'request_new_game')]
+    benchmarks = [
+    (1,fake_new_game),
+    # (3,fake_submit_word),
+    ]
     while True:
         player.primary_loop()
         if benchmarks and time()-start_time > benchmarks[0][0]:
-            getattr(player,benchmarks[0][1])()
+            benchmarks[0][1](player)
+            # getattr(player,benchmarks[0][1])()
             benchmarks.pop(0)
+
 
